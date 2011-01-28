8df440f1073942789b78df8980b771f193a7aa82
pbaker <me@retrodict.com>
2011-01-28 04:37:14 -0500

    Got it to follow certain things

--- em_irc.rb
+++ em_irc.rb
@@ -1,5 +1,6 @@
 require 'mediawiki.rb'
 
+require 'logger'
 require 'rubygems'
 require 'sqlite3'
 require 'bundler/setup'
@@ -8,6 +9,7 @@ require 'bundler/setup'
 require 'eventmachine'
 require 'sinatra/base'
 require 'em-http'
+require 'nokogiri'
 require 'sequel'
 
 
@@ -61,7 +63,7 @@ end
 module Mini
   class IRC < EventMachine::Connection
     include EventMachine::Protocols::LineText2
-    
+    include Mediawiki
     attr_accessor :config, :moderators
     #cattr_accessor :connection
     
@@ -141,13 +143,15 @@ module Mini
     end
     
     def handle_line line
-      operation = proc do
+      process_line = proc do
       	if line =~ Mediawiki::IRC_REGEXP
       	  fields = process_irc(line)
-      	  p fields
-      	  #samples = DB[:samples]
-          #samples << fields
-          #puts dataset.count.to_s
+      	  samples = DB[:samples]
+      	  samples << fields
+      	  if should_follow?(fields[:title])
+      	    puts 'following'
+      	    follow_revision(fields)
+    	    end
     	  end
       end
 
@@ -157,13 +161,35 @@ module Mini
       #end
 
       # Let the thread pool (20 Ruby threads) handle request
-      EM.defer(operation)#, callback)
+      EM.defer(process_line)#, callback)
+    end
+    
+    def follow_revision fields
+      #get the diff and associated data
+      data = get_diff_data(fields[:revision_id])
+      
+      #parse it for links
+      #if there are links, investigate!
+      #EM.defer()
+    end
+    
+    def get_diff_data revision_id
+      #get the xml from wikipedia
+      url = form_url({:prop => :revisions, :revids => rev_id, :rvdiffto => 'prev', :rvprop => 'ids|flags|timestamp|user|size|comment|parsedcomment|tags|flagged' })
+      EM::HttpRequest.new(url).get.callback do |http|
+        puts http.response.to_s[0..100]
+      end
+      
+      #parse the xml
+      #test to see if we have a badrevid
+      #pull out the diff_xml (TODO and other stuff)
+      
     end
     
     #given the irc announcement in the irc monitoring channel for en.wikipedia, this returns the different fields
     # 0: title (string), 
     # 1: flags (desc) (string), 
-    # 2: rev_id (integer),
+    # 2: revision_id (rev_id) (integer),
     # 3: old_id (integer)
     # 4: user (string), 
     # 5: byte_diff (integer), 
@@ -176,7 +202,13 @@ module Mini
       fields[2] = fields[2].to_i
       fields[3] = fields[3].to_i
       fields[5] = fields[5].to_i
-      {:title => fields[0], :flags => fields[1], :rev_id => fields[2], :old_id => fields[3], :user => fields[4], :byte_diff => fields[5], :comment => fields[6]}
+      {:title => fields[0], :flags => fields[1], :revision_id => fields[2], :old_id => fields[3], :user => fields[4], :byte_diff => fields[5], :comment => fields[6]}
+    end
+    
+    #look at title, exclude titles starting with: User talk, Talk, Wikipedia, User, etc.
+    def should_follow? article_title 
+      bad_beg_regex = /^(Talk:|User:|User\stalk:|Wikipedia:|Wikipedia\stalk:|File\stalk:|MediaWiki:|MediaWiki\stalk:|Template\stalk:|Help:|Help\stalk:|Category\stalk:|Thread:|Thread\stalk:|Summary\stalk:|Portal\stalk:|Book\stalk:|Special:|Media:)/
+      !(article_title =~ bad_beg_regex)
     end
     
     def unbind
@@ -199,7 +231,7 @@ module Mini
   end
 end
 
-DB = Sequel.sqlite "en_wikipedia.sqlite"
+DB = Sequel.sqlite "en_wikipedia.sqlite", :logger => Logger.new('log/db.log')
 unless DB.table_exists?(:samples)
   DB.create_table :samples do
     primary_key :id #autoincrementing primary key
@@ -213,6 +245,27 @@ unless DB.table_exists?(:samples)
     #DateTime :created, :default => :'(datetime(\'now\'))'.sql_function() #TODO
   end
 end
+unless DB.table_exists?(:links)
+  DB.create_table :links do
+    primary_key :id #autoincrementing primary key
+    String :source
+    String :headers
+    String :url
+    Integer :revision_id
+    String :wikilink_description
+    #DateTime :created, :default => :'(datetime(\'now\'))'.sql_function() #TODO
+  end
+end
+
+# @samples = DB[:samples]
+# @samples << {
+#   :comment=>"Robot: Listifying from Category:Candidates for speedy deletion (51 entries)", 
+#   :title=>"User:Cyde/List of candidates for speedy deletion/Subpage", 
+#   :flags=>"MB", 
+#   :revision_id=>410531074, 
+#   :old_id=>410530660, 
+#   :byte_diff=>130, :user=>"Cydebot"}
+# puts @samples.count
 
 Mini::Bot.start(
   :secret => 'GHMFQPKNANMNTHQDECECSCWUCMSNSHSAFRGFTHHD',
@@ -225,7 +278,7 @@ Mini::Bot.start(
   :channels => ['en.wikipedia']#[*channels]
 )
 
-#DB = Sequel.connect('sqlite://blog.db')
+#DB = Sequel.connect('sqlite://blog.db', :logger => Logger.new('log/db.log'))
 
 # :default => :'datetime(\'now\',\'localtime\')'.sql_function
 # DATE DEFAULT (datetime('now','localtime'))
