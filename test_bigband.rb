60b24b14e7ed20d73e5b6b2b86cefac2f91a4d1a
pbaker <me@retrodict.com>
2011-02-10 16:45:16 -0500

    got locals working with hierarchical templates

--- test_bigband.rb
+++ test_bigband.rb
@@ -2,6 +2,8 @@ require 'rubygems'
 require 'bundler/setup'
 require 'sinatra/base'
 require 'sinatra/reloader'
+require 'sinatra/advanced_routes'
+require 'sequel'
 
 class MyApp < Sinatra::Base
   configure do
@@ -9,32 +11,53 @@ class MyApp < Sinatra::Base
     register Sinatra::Reloader
   end
   
-  get '/' do
-    'helloworld and me'
+  helpers do #accessible by the templates too...
+    #returns a hash with paths as keys and route info in another hash keyed by name/value pairs
+    def routes
+      routes = {}
+      self.class.each_route do |route|
+        #routes[:name] = route.app.name   # "SomeSinatraApp"
+        info = {}
+        routes[route.path.to_s.to_sym] = info        # that's the path given as argument to get and akin
+        info[:verb] = route.verb       # get / head / post / put / delete
+        info[:file] = route.file       # "some_sinatra_app.rb" or something
+        info[:line] = route.line       # the line number of the get/post/... statement
+        info[:pattern] = route.pattern    # that's the pattern internally used by sinatra
+        info[:keys] = route.keys       # keys given when route was defined
+        info[:conditions] = route.conditions # conditions given when route was defined
+        info[:block] = route.block      # the route's closure
+      end
+      routes
+    end
   end
   
+  before do
+    @db = Sequel.sqlite 'en_wikipedia.sqlite.bak'
+    #@db[:samples]
+    #@db[:links]
+  end
+  
+  get '/' do
+    @site = 'site'
+    @title = 'title'
+    @masthead = 'masthead'
+    erb(:index, :locals => {:body => 'helloo'})#, {:locals => { :body => 'helloworld and me' }}, {:locals => { :body => 'helloworld and me' }})
+  end
+  
+  get '/map' do
+    routes().keys.join(' ').to_s
+  end
 end
 
 MyApp.run!
 
 # routes
 # require "sinatra/base"
-# require "sinatra/advanced_routes"
+# 
 # 
 # class Foo < Sinatra::Base
 #   register Sinatra::AdvancedRoutes
 # end
 # require "some_sinatra_app"
 # 
-# SomeSinatraApp.each_route do |route|
-#   puts "-"*20
-#   puts route.app.name   # "SomeSinatraApp"
-#   puts route.path       # that's the path given as argument to get and akin
-#   puts route.verb       # get / head / post / put / delete
-#   puts route.file       # "some_sinatra_app.rb" or something
-#   puts route.line       # the line number of the get/post/... statement
-#   puts route.pattern    # that's the pattern internally used by sinatra
-#   puts route.keys       # keys given when route was defined
-#   puts route.conditions # conditions given when route was defined
-#   puts route.block      # the route's closure
-# end
+
